7fae91dbdb475cb09b3450683982296ed1134692
f.sedov <f.sedov@tinkoff.ru>
2024-11-26 11:02:16 +0300

    Lesson

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,24 +1,69 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.example.myapplication.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int variable = 0;
+
+    private ActivityMainBinding binding;
+
+    public MainActivity() {
+        // super(R.layout.activity_main);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        // setContentView(R.layout.activity_second);
+
+        // View view = getLayoutInflater().inflate(R.layout.activity_second, null);
+        // setContentView(view);
+
+        // TextView elementOne = findViewById(R.id.elementOne);
+        // String text = elementOne.getText().toString();
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        String text = binding.elementOne.toString();
+        Log.d("MY_APP_DEBUG_TAG", "Element one text: " + text);
+
+        // Context applicationContext = getApplicationContext();
+        // Context activityContext = this;
+        // Activity = Context
+
+        // String appName = activityContext.getResources().getString()
+
+
+        /*binding.elementOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });*/
+
+        String textFromResources = getString(R.string.element_text);
+
+        binding.elementOne.setOnClickListener(view -> {
+            variable += 1;
+            String textFormatted = String.format(textFromResources, variable);
+            binding.elementOne.setText(textFormatted);
+        });
+
+        // binding.elementOne.setText(R.string.element_text);
+
+        binding.elementTwo.setOnClickListener(v -> {
+            // Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+            // startActivity(intent);
+            Intent intent = new Intent(this, SecondActivity.class);
+            startActivity(intent);
         });
     }
 }
